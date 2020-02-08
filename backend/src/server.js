c0c4956378a1fbb12d340abdaafd48f7cdb38239
ColeyG <cjgeerts@gmail.com>
2020-02-08 17:09:34 -0500

    Server Config

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const httpServer = http.createServer(app);
-const config = require('./config.json');
+const config = require('../config/config.json');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +13,7 @@ httpServer.listen(3000, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('../routes/index');
 
 app.use(session({ secret: config.sessionSecret, saveUninitialized: true, resave: true }));
 app.use(cookieParser());
