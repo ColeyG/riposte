38883df455ad92ad60a7db547ca260df1f74a7e4
ColeyG <cjgeerts@gmail.com>
2020-04-20 00:39:52 -0400

    Final

--- scripts/pixelate.sh
+++ scripts/pixelate.sh
@@ -1,8 +1,5 @@
 basedir=$(pwd)/assets
 
-# convert -scale 10% -colors 16 $basedir/bliss.png $basedir/newimage.png
-# convert -scale 1000% $basedir/newimage.png $basedir/newimage.png
-
 for file in $basedir/downsize/*;
 do
   filename=$(echo "$file" | sed 's/downsize/pixelated/g')
